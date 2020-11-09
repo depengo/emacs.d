9b52bdd72872f9ac7bc7cf62d21a48d19180d746
depengo <74166913+depengo@users.noreply.github.com>
2020-11-09 12:00:49 +0800

    Create init.el

---
+++ init.el
@@ -0,0 +1,3 @@
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize) ;; You might already have this line
